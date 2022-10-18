792ce7014bb3fe29ca19c4dbc405794a6ca2389c
Royce Strange <rrrbbbsss@ryzst.net>
2022-10-18 09:10:26 -0500

    finish converting to graphql

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Jumbotron,
   Container,
@@ -14,38 +14,10 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  //const [userData, setUserData] = useState({});
   const [removeBook] = useMutation(REMOVE_BOOK);
   const { loading, data } = useQuery(QUERY_ME);
 
   const userData = data?.me || {};
-  // use this to determine if `useEffect()` hook needs to run again
-  //const userDataLength = Object.keys(userData).length;
-
-  //useEffect(() => {
-  //  const getUserData = async () => {
-  //    try {
-  //      const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //      if (!token) {
-  //        return false;
-  //      }
-
-  //      const response = await getMe(token);
-
-  //      if (!response.ok) {
-  //        throw new Error("something went wrong!");
-  //      }
-
-  //      const user = await response.json();
-  //      setUserData(user);
-  //    } catch (err) {
-  //      console.error(err);
-  //    }
-  //  };
-
-  //  getUserData();
-  //}, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -56,7 +28,7 @@ const SavedBooks = () => {
     }
 
     try {
-      await removeBook({ variables: { input: bookId } });
+      await removeBook({ variables: { input: { bookId } } });
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
